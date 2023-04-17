f1743929954ec75ec2965e8b8f109df3dad8b2ec
MAkif2004 <akif.golge@gmail.com>
2023-04-17 17:59:40 +0200

    made admin toggle

--- FakeNews/app/Http/Controllers/Auth/RegisteredUserController.php
+++ FakeNews/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,13 +34,14 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'isadmin' => ['required', 'integer', 'max:1']
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'is_admin' => 0
+            'is_admin' => $request->isadmin
         ]);
 
         event(new Registered($user));
